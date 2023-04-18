e34bee70ee0cf02bec0de80614873bb5c47068ef
tafforzore <nzomutchawilfrid@gmail.com>
2023-04-18 18:01:17 +0200

    calcualtrice
    realiser a but experimentale

--- apps.js
+++ apps.js
@@ -1,7 +1,6 @@
 var number = document.querySelector('.number').children
 var action = document.querySelector('.action').children
 let currentValue
-let currentValue2 = ''
 
 
 for (var i=0; i<=number.length-1; i++){
@@ -14,31 +13,33 @@ for (var i=0; i<=number.length-1; i++){
 
 for (var i=0; i<=action.length-1; i++){
 	action[i].addEventListener('click', function(){
-		let a = document.querySelector('input').value
+		let a = document.querySelector('input')
 		if(this == document.querySelector('#del')){
-		
+			document.querySelector('input').value = a.value.slice(0,-1)
 		}
 		//operation arithmetique
-		if(this == document.querySelector('butonpl')){
-			curerentValue = document.querySelector('.textinput')
-			document.querySelector('.textinput').value =''
-			if(currentValue2 != ''){
-				currentValue +=currentValue2
-				document.querySelector('.textinput').value = currentValue
-			}
+		if(this == document.querySelector('#butonpl')){
+			document.querySelector('input').value = a.value+this.textContent
+		}
+		if(this == document.querySelector('#butonmoins')){
+			document.querySelector('input').value = a.value+this.textContent
+		}
+		if(this == document.querySelector('#butonm')){
+			document.querySelector('input').value = a.value+this.textContent
+		}
+		if(this == document.querySelector('#butond')){
+			document.querySelector('input').value = a.value+this.textContent
 		}
 
-
-
-
-		if(this == document.querySelector('#ac')){
+		if(this == document.querySelector('.ac')){
 			document.querySelector('input').value = ''
 		}
 		if(this == document.querySelector('#result')){
-			document.querySelector('input').value = ''
-		}else{
-			document.querySelector('input').value = a+this.textContent
+			a.value = eval(document.querySelector('input').value)
+			document.querySelector('input').value = a.value
 		}
 	})
 }
+
+
 
